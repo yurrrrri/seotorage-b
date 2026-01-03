11f6beccc7df89c959755e715dd6d9c1181d0a98
yurison <yu_ri_son@naver.com>
2026-01-03 22:16:54 +0900

    feat: added some findBy... methods

---
+++ src/main/java/xyz/seotorage/controller/BookController.java
@@ -0,0 +1,26 @@
+package xyz.seotorage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.seotorage.domain.Book;
+import xyz.seotorage.domain.dto.AladinSearchRequest;
+import xyz.seotorage.service.AladinApiService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/aladin")
+public class AladinController {
+
+    private final AladinApiService aladinApiService;
+
+    @GetMapping("/search")
+    public List<Book> searchBooks(@RequestBody AladinSearchRequest req) {
+        return aladinApiService.searchBooks(req);
+    }
+
+}
